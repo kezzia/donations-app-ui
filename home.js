33cf7b2a0dbca00a58affd403e98e568bb605e36
undeadVivisector <mvandertramps@gmail.com>
2016-11-28 16:57:54 -0500

    Basics of the electron desktop UI. It can accept and store localy strings and an image file supplied by the user. It has no CSS, and no Angular as of yet and the existing JavaScript is exceedingly ugly. C'est la vie :/

---
+++ home.js
@@ -0,0 +1,26 @@
+
+var button = document.createElement('button')
+button.textContent = 'Submit'
+
+button.addEventListener('click', () => {
+  var project_name = document.getElementById("name").value
+  /*var project_image = document.getElementById("image").value*/
+  var project_category = document.getElementById("category").value
+  var project_description = document.getElementById("description").value
+  var project_goal = document.getElementById("goal").value
+
+  console.log(project_name)
+  console.log(project_image)
+  console.log(project_category)
+  console.log(project_description)
+  console.log(project_goal)
+
+  document.getElementById("name").value = ""
+  document.getElementById("image").value = ""
+  document.getElementById("category").value = ""
+  document.getElementById("description").value = ""
+  document.getElementById("goal").value = ""
+}, false)
+
+document.body.appendChild(button)
+
