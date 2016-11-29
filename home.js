e689ef22fe0455e20b9402c0df7b70d874b64c93
Acespace <alston.clark17@gmail.com>
2016-11-29 17:45:31 -0500

    adding bootstrap, making things look better

--- home.js
+++ home.js
@@ -1,25 +1,23 @@
 
-var button = document.createElement('button')
-button.textContent = 'Submit'
 
-button.addEventListener('click', () => {
-  var project_name = document.getElementById("name").value
-  /*var project_image = document.getElementById("image").value*/
-  var project_category = document.getElementById("category").value
-  var project_description = document.getElementById("description").value
-  var project_goal = document.getElementById("goal").value
 
-  console.log(project_name)
-  console.log(project_category)
-  console.log(project_description)
-  console.log(project_goal)
+function submitted(){
 
-  document.getElementById("name").value = ""
-  document.getElementById("category").value = ""
-  document.getElementById("description").value = ""
-  document.getElementById("goal").value = ""
-  location.href = "./submitted.html";
+    var project_name = document.getElementById("name").value
+    /*var project_image = document.getElementById("image").value*/
+    var project_category = document.getElementById("category").value
+    var project_description = document.getElementById("description").value
+    var project_goal = document.getElementById("goal").value
 
-}, false)
+    console.log(project_name)
+    console.log(project_category)
+    console.log(project_description)
+    console.log(project_goal)
 
-document.body.appendChild(button)
+    document.getElementById("name").value = ""
+    document.getElementById("category").value = ""
+    document.getElementById("description").value = ""
+    document.getElementById("goal").value = ""
+    location.href = "./submitted.html"
+
+};
